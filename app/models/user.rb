6417f9caebad21b00b93fe3ba31f213ea721587e
ads-andredesantana <60289979+ads-andredesantana@users.noreply.github.com>
2020-10-16 18:38:51 -0400

    Revert "Friday afternoon"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,7 @@
-class User < ActiveRecord::Base
-   #acts_as_token_authenticatable
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   #:confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   extend Devise::Models
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-          
-  
-
-
-  def username
-    return self.email.split('@'[0].capitalize)
-  end
-  
 end
