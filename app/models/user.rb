e18ab78b33e49da56931029bbade1fb46a0d6c44
Emmanuella <emmanuella05@yahoo.com>
2020-10-16 17:07:08 -0400

    Friday afternoon

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+   #acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   #:confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   extend Devise::Models
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+          
+  
+
+
+  def username
+    return self.email.split('@'[0].capitalize)
+  end
+  
 end
