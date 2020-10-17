4a5519e6425d29ccf7f86ca14a1bc6b1c6fcf711
olivier <olivier_beauchesne4@hotmail.com>
2020-10-16 20:17:14 -0400

    4 linge

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+#root
 Rails.application.routes.draw do
   root 'rocket_elevator#index'
-
+  get 'rocket_elevator/residancial'
+  get 'rocket_elevator/comercial'
+  get 'rocket_elevator/quote'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
