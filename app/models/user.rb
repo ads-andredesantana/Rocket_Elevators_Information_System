83f94f362c0a3560b7fe6d65447a16109a25c3a8
Emmanuella <emmanuella05@yahoo.com>
2020-10-19 11:08:12 -0400

    working branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  belongs_to :empployees
   #acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
