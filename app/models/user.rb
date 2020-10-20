a94187ec6d364f4b439517b0452fa8def06bf5bc
Andre De Santana <ads.andredesantana@gmail.com>
2020-10-20 10:20:10 -0400

    user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  belongs_to :empployees
+  #belongs_to :empployees
   #acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
