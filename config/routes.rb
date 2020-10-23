97d19c16680a6431af64ea583283f5622713df2b
olivier <olivier_beauchesne4@hotmail.com>
2020-10-23 10:55:02 -0400

    2020/10/23/4

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,11 @@ Rails.application.routes.draw do
   get "/commercial", to: "rocket_elevator#commercial"
   get "/quote", to: "rocket_elevator#quote"
   get "/news_press1", to: "rocket_elevator#news_press1"
+  get "/news_press2", to: "rocket_elevator#news_press2"
+  get "/news_press3", to: "rocket_elevator#news_press3"
+  get "/news_press4", to: "rocket_elevator#news_press4"
+  get "/news_press5", to: "rocket_elevator#news_press5"
+  get "/news_press6", to: "rocket_elevator#news_press6"
   get "/portfolio_detail_modal_1.html", to: "rocket_elevators#portfolio_detail_modal_1"
 
   # get "/login", to: ""
